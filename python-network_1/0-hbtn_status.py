9ea4e7d4bee3ad02b5c1e21e1e50374ca710ccb7
lasgedu <c.unekwe@alustudent.com>
2023-11-29 00:17:27 -0800

    all

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,9 +10,8 @@ url = "https://alu-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     content = response.read()
     utf8_content = content.decode('utf-8')
-
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
-print(f"\t- utf8 content: {utf8_content}")
-
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
