411b9bee84867f3e1ec3e4b04dab3b732dc832dc
lasgedu <c.unekwe@alustudent.com>
2023-11-29 00:23:42 -0800

    no1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-Fetches https://alu-intranet.hbtn.io/status
-"""
-
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
